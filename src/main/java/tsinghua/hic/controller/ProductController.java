9f0eed9aac1e26f82392969d1a7769f4eb4decaf
hi1summer <gonick@163.com>
2021-01-12 17:37:41 +0800

    UPDATE

--- src/main/java/tsinghua/hic/controller/ProductController.java
+++ src/main/java/tsinghua/hic/controller/ProductController.java
@@ -1,5 +1,6 @@
 package tsinghua.hic.controller;
 
+import java.io.File;
 import java.net.InetAddress;
 import java.text.SimpleDateFormat;
 import java.util.Arrays;
@@ -7,16 +8,26 @@ import java.util.Date;
 import java.util.List;
 import java.util.Optional;
 
+import org.json.JSONObject;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.FileSystemResource;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
 import org.springframework.ui.ModelMap;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
+import org.springframework.util.ResourceUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.ModelAndView;
 
 import net.bytebuddy.asm.Advice.This;
@@ -123,11 +134,6 @@ public class ProductController {
             ModelMap model) {
         Productinfo productinfo = new Productinfo();
         productinfo.setGid(gid);
-        Date now = new Date();
-        SimpleDateFormat formatter = new SimpleDateFormat(
-                "yyyy-MM-dd HH:mm:ss");
-        productinfo.setCreateTime(formatter.format(now));
-        productinfo.setUpdateTime(formatter.format(now));
         productinfo.setLoc("深圳");
         productinfo.setInfoType((short) InfoTypeEnum.Producer.getTypeIndex());
         model.addAttribute("productinfo", productinfo);
@@ -139,11 +145,6 @@ public class ProductController {
             ModelMap model) {
         Productinfo productinfo = new Productinfo();
         productinfo.setGid(gid);
-        Date now = new Date();
-        SimpleDateFormat formatter = new SimpleDateFormat(
-                "yyyy-MM-dd HH:mm:ss");
-        productinfo.setCreateTime(formatter.format(now));
-        productinfo.setUpdateTime(formatter.format(now));
         productinfo.setLoc("深圳");
         productinfo.setInfoType((short) InfoTypeEnum.Express.getTypeIndex());
         model.addAttribute("productinfo", productinfo);
@@ -155,15 +156,37 @@ public class ProductController {
             ModelMap model) {
         Productinfo productinfo = new Productinfo();
         productinfo.setGid(gid);
-        Date now = new Date();
-        SimpleDateFormat formatter = new SimpleDateFormat(
-                "yyyy-MM-dd HH:mm:ss");
-        productinfo.setCreateTime(formatter.format(now));
-        productinfo.setUpdateTime(formatter.format(now));
         productinfo.setLoc("深圳");
         productinfo
                 .setInfoType((short) InfoTypeEnum.Transaction.getTypeIndex());
         model.addAttribute("productinfo", productinfo);
+
+        List<Productinfo> productinfos = productService
+                .findProductinfoByGid(gid);
+        model.addAttribute("productinfos", productinfos);
+
+        Product product = productService.get(gid).get();
+        model.addAttribute("product", product);
+
+        return new ModelAndView("addproductinfo", model);
+    }
+
+    @GetMapping("/customer/{gid}")
+    public ModelAndView customer(@PathVariable(required = true) String gid,
+            ModelMap model) {
+        Productinfo productinfo = new Productinfo();
+        productinfo.setGid(gid);
+        productinfo.setLoc("深圳");
+        productinfo.setInfoType((short) InfoTypeEnum.VerifyUser.getTypeIndex());
+        model.addAttribute("productinfo", productinfo);
+
+        List<Productinfo> productinfos = productService
+                .findProductinfoByGid(gid);
+        model.addAttribute("productinfos", productinfos);
+
+        Product product = productService.get(gid).get();
+        model.addAttribute("product", product);
+
         return new ModelAndView("addproductinfo", model);
     }
 
@@ -174,7 +197,13 @@ public class ProductController {
             if (productinfo.getContent().strip().isEmpty()) {
                 throw new Exception("产品信息内容不能为空");
             } else {
+                Date now = new Date();
+                SimpleDateFormat formatter = new SimpleDateFormat(
+                        "yyyy-MM-dd HH:mm:ss");
+                productinfo.setCreateTime(formatter.format(now));
+                productinfo.setUpdateTime(formatter.format(now));
                 productService.add(productinfo);
+                System.out.println(productinfo.getInfoType());
                 hashGenerateService.generate(productinfo.getProductinfoid());
                 model.addAttribute("result", "添加成功");
             }
@@ -218,6 +247,18 @@ public class ProductController {
         return new ModelAndView("verifyuser", model);
     }
 
+    @GetMapping("/demodata/{gid}")
+    public ModelAndView demodata(@PathVariable(required = true) String gid,
+            ModelMap model) {
+        Product product = productService.get(gid).get();
+        List<Productinfo> productinfos = productService
+                .findProductinfoByGid(gid);
+
+        model.addAttribute("product", product);
+        model.addAttribute("productinfos", productinfos);
+        return new ModelAndView("verifyuser", model);
+    }
+
     @GetMapping(value = "/hash/{productinfoid}")
     @ResponseBody
     public String getHash(@PathVariable(required = true) String productinfoid) {
@@ -228,5 +269,80 @@ public class ProductController {
         } else {
             return productinfohashOptional.get().getHash();
         }
+    }
+
+    @GetMapping(value = "/result/{gid}")
+    @ResponseBody
+    public ModelAndView result(@PathVariable(required = true) String gid,
+            ModelMap model) {
+        try {
+            Product product = productService.get(gid).get();
+            model.addAttribute("product", product);
+        } catch (Exception e) {
+            log.error(e.getLocalizedMessage());
+        }
+
+        model.addAttribute("gid", gid);
+        model.addAttribute("result", "检验成功！");
+        model.addAttribute("result2", "产品ID: " + gid);
+        return new ModelAndView("scanresult", model);
+    }
+
+    @GetMapping(value = "/generateqrcode")
+    @ResponseBody
+    public String generateqrcode() throws Exception {
+        GenerateQRCode generateQRCode = new GenerateQRCode();
+        String srcString = generateQRCode.QREncode(300, 300,
+                "http://www.tsinghuahic.cn/", "website");
+        return srcString;
+    }
+
+    @PostMapping(value = "/upload")
+    @ResponseBody
+    public String upload(MultipartFile file) throws Exception {
+        String filenameString = ResourceUtils.getURL("").getPath()
+                + +System.currentTimeMillis() + file.getOriginalFilename();
+        File tempFile = new File(filenameString);
+        file.transferTo(tempFile);
+        FileSystemResource resource = new FileSystemResource(tempFile);
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.MULTIPART_FORM_DATA);
+        headers.setCacheControl("no-cache");
+        MultiValueMap<String, Object> param = new LinkedMultiValueMap<>();
+        param.add("file", resource);
+        HttpEntity<MultiValueMap<String, Object>> files = new HttpEntity<>(
+                param, headers);
+        RestTemplate restTemplate = new RestTemplate();
+        String result = restTemplate
+                .postForEntity("http://hic.vipgz6.idcfengye.com/upload", files,
+                        String.class)
+                .getBody();
+
+        JSONObject jsonObject = new JSONObject(result);
+        Integer codInteger = jsonObject.getInt("code");
+        String msgString = jsonObject.getString("msg");
+        String dataString = jsonObject.getString("data");
+        if (codInteger == 0) {
+            Optional<Product> product = productService.get(dataString);
+            if (!product.isEmpty()) {
+                Product actualProduct = product.get();
+                jsonObject.put("id", codInteger);
+                jsonObject.put("successcontent",
+                        actualProduct.getProductName() + "\n"
+                                + actualProduct.getGid() + "\n"
+                                + actualProduct.getCreateTime() + "\n"
+                                + actualProduct.getUpdateTime());
+                jsonObject.put("bottomtime", actualProduct.getCreateTime()
+                        + "\n" + actualProduct.getUpdateTime());
+                jsonObject.put("bottomcount", "2次");
+            } else {
+
+            }
+        } else {
+
+        }
+        tempFile.delete();
+        return jsonObject.toString();
     }
 }
